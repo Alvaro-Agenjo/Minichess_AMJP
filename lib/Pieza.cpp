bfbb663b7e11122e109f204f31d6ee4f2fb7c97e
UnarmingHat72 <159081671+UnarmingHat72@users.noreply.github.com>
2024-04-27 08:43:39 -0800

    Para que amenace todo detras de primerapieza

--- lib/Pieza.cpp
+++ lib/Pieza.cpp
@@ -14,7 +14,7 @@ void Pieza::crearTablero(Tablero* t)
 
 void Pieza::almacenarPiezaMemoria()
 {
-	_tab->getTablero()[_posicion.y][_posicion.x].setOcupacion(_tpieza); //se ha movido la clase interaccion y sus funcione slas adopta pieza, que tiene una referencia al tablero
+	_tab->getTablero()[_posicion.y][_posicion.x].setOcupacion(_tpieza); //se ha movido la clase interaccion y sus funciones las adopta pieza, que tiene una referencia al tablero
 	_tab->getTablero()[_posicion.y][_posicion.x].setPuedeMover(0);
 }
 
@@ -23,26 +23,25 @@ bool Pieza::validarCasilla(Vector2D coord)
 	Casilla &c = _tab->getTablero()[coord.y][coord.x];		//enlace a direccion de casilla a comprobar
 	Vector2D negativo{ -1,-1 };
 	Vector2D dim = _tab->getDimension();
-	static bool primerapieza = 1, segundapieza =1;			//detector de piezas clavadas, se puede mover hasta la primera pieza,a esta se la puede comer, y amenaza todo hasta encontrarse una segunda pieza
-	if (negativo < coord  && coord < dim)		//es decir la posicion se encuentra dentro de los límites del tablero
-	{
+	static bool primerapieza = 1, segundapieza =0;			//detector de piezas clavadas, se puede mover hasta la primera pieza,a esta se la puede comer, y amenaza todo hasta encontrarse una segunda pieza
+	if (negativo < coord  && coord < dim)		//comprobar que la posicion se encuentra dentro de los límites del tablero
+	{                                         
 
-		if (c.getOcupacion() == 0)                       //comprueba si hay alguien en la casilla
+		if (c.getOcupacion() == 0)                       //comprueba si hay alguien en la casilla, en este caso lo está
 		{
-			//std::cout << "La casilla (" << c.getPosicion().x << "," << c.getPosicion().y << ") está vacía\n";
-			c.setAtacada(!primerapieza && !segundapieza);
-			c.setPuedeMover(primerapieza);
+			c.setAtacada(!primerapieza && !segundapieza); //marca la segundapieza como amenazada
+			c.setPuedeMover(primerapieza); //se puede mover a esa dado que no se ha encontrado la primera pieza todavía
 			return true;
 		}
 		else if ((c.getOcupacion() < 0 && _jugador == J1) || (c.getOcupacion() > 0 && _jugador == J2))       //si lo hay comprueba que jugador está ahí situado
-		{                                           //si la pieza en dichas coords es enemiga se habilita la posibilidad de comer (true)
+		{                                           //si la pieza en dichas coords es enemiga se habilita la posibilidad de comer (true), ocupacion + J1 y ocupacion - J2
 			if (!primerapieza) { segundapieza = 1; }
-			c.setAtacada(primerapieza|| segundapieza);        //   <----
+			c.setAtacada(primerapieza|| segundapieza);        
 			c.setPuedeMover(primerapieza);
 			primerapieza = 0;
 			return true;
 		}
-		else                                        // si en la casilla hay una ficha aliada se prohibe el movimiento y el retorno es (false)
+		else                   // si en la casilla hay una ficha aliada se prohibe el movimiento y el retorno es (false)
 		{
 			primerapieza=1;
 			return false;
